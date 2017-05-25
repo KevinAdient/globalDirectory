a2baa7b22c880c36fbcbfacf7066bb727c75614f
KevinAdient <Kun@kevins-macbook-pro.local>
2017-05-25 11:49:34 -0400

    add shadow on image view's superview in detail person view

--- Directory/PeopleDetailViewController.swift
+++ Directory/PeopleDetailViewController.swift
@@ -81,18 +81,18 @@ class PeopleDetailViewController: UIViewController {
         
         let underlineAttributeEmail = [NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue]
         let underlineAttributedStringEmail = NSAttributedString(string: currentPerson.email!, attributes: underlineAttributeEmail)
-        currentPersonEmailLbl.font = UIFont(name: "Domus-Regular", size: 14)
+        currentPersonEmailLbl.font = UIFont(name: "Domus-Regular", size: 15)
         currentPersonEmailLbl.attributedText = underlineAttributedStringEmail
         
         
         let underlineAttributePhone = [NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue]
         let underlineAttributedStringPhone = NSAttributedString(string: currentPerson.deskphone!, attributes: underlineAttributePhone)
-        currentPersonPhoneLbl.font = UIFont(name: "Domus-Regular", size: 14)
+        currentPersonPhoneLbl.font = UIFont(name: "Domus-Regular", size: 15)
         currentPersonPhoneLbl.attributedText = underlineAttributedStringPhone
         
         let underlineAttributeMobile = [NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue]
         let underlineAttributedStringMobile = NSAttributedString(string: currentPerson.mobilephone!, attributes: underlineAttributeMobile)
-        currentPersonMobileLbl.font = UIFont(name: "Domus-Regular", size: 14)
+        currentPersonMobileLbl.font = UIFont(name: "Domus-Regular", size: 15)
         currentPersonMobileLbl.attributedText = underlineAttributedStringMobile
         
         currentPersionDeptLbl.text = currentPerson.theirDepartment?.departmentName
@@ -100,8 +100,7 @@ class PeopleDetailViewController: UIViewController {
         
         let underlineAttributeAddress = [NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue]
         let underlineAttributedStringAddress = NSAttributedString(string: (currentPerson.theirAddress?.city!)! + " " + (currentPerson.theirAddress?.streetName1)!, attributes: underlineAttributeAddress)
-        currentPersonOfficeLbl
-            .font = UIFont(name: "Domus-Regular", size: 14)
+        currentPersonOfficeLbl.font = UIFont(name: "Domus-Regular", size: 15)
         currentPersonOfficeLbl.attributedText = underlineAttributedStringAddress
         
         
@@ -332,7 +331,44 @@ extension PeopleDetailViewController : MFMailComposeViewControllerDelegate {
     
 }
 
-
+extension UIView {
+    
+    @IBInspectable var shadow: Bool {
+        get {
+            return layer.shadowOpacity > 0.0
+        }
+        set {
+            if newValue == true {
+                self.addShadow()
+            }
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+            
+            // Don't touch the masksToBound property if a shadow is needed in addition to the cornerRadius
+            if shadow == false {
+                self.layer.masksToBounds = true
+            }
+        }
+    }
+    
+    
+    func addShadow(shadowColor: CGColor = UIColor.black.cgColor,
+                   shadowOffset: CGSize = CGSize(width: 1.0, height: 2.0),
+                   shadowOpacity: Float = 0.4,
+                   shadowRadius: CGFloat = 3.0) {
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+    }
+}
 
 
 
